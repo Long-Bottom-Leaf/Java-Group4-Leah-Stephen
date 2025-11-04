525c5e2a7f0d01be0cb55601bd89264a98bfc4a5
Long-Bottom-Leaf <drcheesewizard@gmail.com>
2025-11-03 21:10:51 -0330

    Finished Doctor class

---
+++ MedicationTracking/Doctor.java
@@ -0,0 +1,33 @@
+package MedicationTracking;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Doctor extends Person {
+    private String specialization;
+    private List<Patient> patients;
+
+    public Doctor(String id, String name, int age, String phoneNumber, String specialization) {
+        super(name, age, phoneNumber);
+        this.specialization = specialization;
+        this.patients = new ArrayList<>();
+    }
+
+    // Accessors
+    public String getSpecialization() {
+        return specialization;
+    }
+
+    public List<Patient> getPatients() {
+        return patients;
+    }
+
+    // Mutator
+    public void setSpecialization(String specialization) {
+        this.specialization = specialization;
+    }
+
+    public void addPatient(Patient patient) {
+        this.patients.add(patient);
+    }
+}
